beee58b5cc2bc52635ebf0c27e4b5576770ea73a
travachipdemacaco <luizjoaoprofissional@gmail.com>
2025-05-17 22:59:06 -0300

    tprecursiva

--- vagner.java
+++ vagner.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class main {
-    public main(String[] args) {
+public class vagner {
+    public void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
 
